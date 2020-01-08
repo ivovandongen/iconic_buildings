c0611a41c5fb2e0c2fa03522e0e6946a0a2a9f63
Ivo van Dongen <info@ivovandongen.nl>
2020-01-08 14:19:40 +0200

    added base64 encoded versions of binary gltf models

--- models/convert_glb.sh
+++ models/convert_glb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 for f in *.fbx; do
-  echo "Converting $f"
-  FBX2glTF-darwin-x64 --binary "$f"
+  filename=$(basename -- "$f")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  echo "Converting ${f} to ${filename}.glb"
+  FBX2glTF-darwin-x64 --binary "$f" -o "${filename}"
+  base64 -i "${filename}.glb" -o "${filename}.glb.base64"
 done
